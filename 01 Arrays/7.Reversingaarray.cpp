0edf374a6bc1c68624dd574ca9a2fe88af29b81b
Amit <114171191+Amit-0905@users.noreply.github.com>
2023-03-31 18:13:55 +0530

    Add files via upload

---
+++ 01 Arrays/7.Reversingaarray.cpp
@@ -0,0 +1,31 @@
+// int a[10^6] is max array size we can create
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
