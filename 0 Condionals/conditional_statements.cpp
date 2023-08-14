3ad2e55836bca96e783cf3b7941cd41c09b5f64a
Amit <114171191+Amit-0905@users.noreply.github.com>
2023-08-14 18:23:51 +0530

    conditional_statements.cpp

---
+++ 0 Condionals/conditional_statements.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int bronum;
+    // cout << "Enter Brothers number : " << endl;
+    // cin >> bronum;
+    // if (bronum == 0)
+    // {
+    //     cout << "Baat ban jayegi" << endl;
+    // }
+    // else
+    // {
+    //     cout << "baat nahi banegi" << endl;
+    // }
+    // return 0;
+
+    // **** --> neither of theese is mandatory in for loop
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "DSA practice " << i + 1 << endl;
+    // }
+
+    // +++>>>>> but there will no statement print of loop end and program with stuck at after printing till 4 in loop
+    // so to exit you need to terminate or stop the program execution
+
+    // int i = 0;
+    // for (;;)
+    // {
+    //     if (i < 5)
+    //     {
+    //         cout << i << endl;
+    //         i = i + 1;
+    //     }
+    //     else
+    //     {
+    //         break;
+    //     }
+    // }
+    // cout << "loop end" << endl;
+
+    // accepting input inside if condition
+    // int n;
+    // if (cin >> n)
+    // {
+    //     cout << "amit" << endl;
+    // }
+
+    // if (cout << "cout" << endl) // no ; nis used
+    // {
+    //     cout << "hacker" << endl;
+    // }
+
+    // return 0;
+}
