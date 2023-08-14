83c2e9a3e245992f5c4151980fa800837e4a9980
Amit <114171191+Amit-0905@users.noreply.github.com>
2023-08-15 00:37:16 +0530

    Create Patterns_2.cpp

---
+++ 00 Patterns/Patterns_2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //  ---------------------- Pyramid FUll--------------------------------------------
+    // int n;
+    // cin >> n;
+    // for (int row = 0; row < n; row++)
+    // {
+    //     int k = 0;
+    //     for (int col = 0; col < (2 * n) - 1; col++)
+    //     {
+    //         if (col < n - row - 1)
+    //         {
+    //             cout << " ";
+    //         }
+    //         else if (k < row + 1)
+    //         {
+    //             cout << "* ";
+    //             k++;
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    // ------------------------------------ Inverted Pyramid -------------------------------
+    // int n;
+    // cin >> n;
+    // for (int row = 0; row < n; row++)
+    // {
+    //     // space
+    //     for (int i = 0; i < row; i++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int i = 0; i < n - row; i++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -------------------------------- Diamond🔷 --------------------------------------
+    // int n;
+    // cin >> n;
+    // // upper pyramid
+    // for (int row = 0; row < n; row++)
+    // {
+    //     // space
+    //     for (int col = 0; col < n - row - 1; col++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int i = 0; i < row + 1; i++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // // lower pyramid
+    // for (int row = 0; row < n; row++)
+    // {
+    //     // spaces
+    //     for (int i = 0; i < row; i++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     // stars
+    //     for (int i = 0; i < n - row; i++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
